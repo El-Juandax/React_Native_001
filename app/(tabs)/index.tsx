2ae00647fa2750dfec43406535f1ebf543bc5034
Mari <hernandezjessika777@gmail.com>
2024-08-01 08:44:25 -0500

    App de hoja de vida con login y register

---
+++ app/(tabs)/index.tsx
@@ -0,0 +1,20 @@
+import React, { Component } from 'react'
+import { Text, View } from 'react-native'
+import HojaVida from '@/src/components/hojaVida/HojaVida'
+import Login from '@/src/components/Login/Login'
+import RegisterComponent from '@/src/components/registro/RegisterComponet'
+
+export class index extends Component {
+  render() {
+    return (
+
+      
+        <RegisterComponent/>
+      
+
+
+    )
+  }
+}
+
+export default index
